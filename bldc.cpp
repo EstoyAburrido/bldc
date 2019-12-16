6394335544cc9affe4f56fce5bf0a41588db46d1
EstoyAburrido <54527545+EstoyAburrido@users.noreply.github.com>
2019-12-16 15:39:31 +0300

    Update bldc.cpp

--- bldc.cpp
+++ bldc.cpp
@@ -1,23 +1,14 @@
 #include "bldc.h"
 
 
-BLDC::BLDC() {}
-
-BLDC::BLDC(int TAH, int TAL, int TBH, int TBL, int TCH, int TCL, int HallA, int HallB, int HallC) {
-  _TAH = TAH;
-  _TAL = TAL;
-  _TBH = TBH;
-  _TBL = TBL;
-  _TCH = TCH;
-  _TCL = TCL;
-  _HallA = HallA;
-  _HallB = HallB;
-  _HallC = HallC;
-  pinMode(_HallA, INPUT_PULLUP); 
-  pinMode(_HallB, INPUT_PULLUP); 
+BLDC::BLDC(int TAH, int TAL, int TBH, int TBL, int TCH, int TCL, int HallA, int HallB, int HallC) :
+  _TAH(TAH),  _TAL(TAL),  _TBH(TBH),  _TBL(TBL),  _TCH(TCH),  _TCL(TCL),  _HallA(HallA),  _HallB(HallB),  _HallC(HallC)
+{
+  pinMode(_HallA, INPUT_PULLUP); // pinmode depends of actual circuit
+  pinMode(_HallB, INPUT_PULLUP); // other modes might required by different circuits
   pinMode(_HallC, INPUT_PULLUP); 
   pinMode(_TAH, OUTPUT);
-  pinMode(_TAL, PWM);
+  pinMode(_TAL, PWM); //only lower hald-bridge transistors are working in PWM mode
   pinMode(_TBH, OUTPUT);
   pinMode(_TBL, PWM);
   pinMode(_TCH, OUTPUT);
@@ -45,8 +36,8 @@ void BLDC::setOffset(int offset)
   myinterrupt();
 }
 
-void BLDC::setVals(bool vala, bool valb, bool valc)
-{
+void BLDC::setVals(bool vala, bool valb, bool valc) //временный костыль,
+{// будет убран когда в контроллере будет собран low pass фильтр и значения с датчиков холла можно будет считывать напрямую
 	_vala = vala;
 	_valb = valb;
 	_valc = valc;	
@@ -71,7 +62,9 @@ void BLDC::myinterrupt(){
     }
     if(_vala && !_laststate && millis() - _tmpmillis > 4) { //когда состояние датчика A сменилось с 0 на 1
     	_rpm = millis() - _tmpmillis;
-    	_rpm = (60000 / _rpm)/4;
+    	_rpm = (60000 / _rpm)/4; // the number 4 here because in the motor we've been using there are 4 poles.
+	    			// if you have a different number of poles you should change this number, otherwise
+	    			// you'd get wrong RPM value
     	_tmpmillis = millis();
     }
     _laststate = _vala;
@@ -91,31 +84,24 @@ void BLDC::setStep(int step){
   {
       case 1:
           switchPhase(1, 0, 0, 1, 0, 0);
-         // Serial.println("case:1");
 		  break;
       case 2:
-          switchPhase(1, 0, 0, 0, 0, 1);          
-         // Serial.println("case:2");
+          switchPhase(1, 0, 0, 0, 0, 1);    
 		  break;
       case 3:
-          switchPhase(0, 0, 1, 0, 0, 1);          
-         // Serial.println("case:3");
+          switchPhase(0, 0, 1, 0, 0, 1);   
 		  break;
       case 4:
-          switchPhase(0, 1, 1, 0, 0, 0);          
-         // Serial.println("case:4");
+          switchPhase(0, 1, 1, 0, 0, 0);    
 		  break;
       case 5:
-          switchPhase(0, 1, 0, 0, 1, 0);          
-         // Serial.println("case:5");
+          switchPhase(0, 1, 0, 0, 1, 0);   
 		  break;
       case 6:
-          switchPhase(0, 0, 0, 1, 1, 0);          
-         // Serial.println("case:6");
+          switchPhase(0, 0, 0, 1, 1, 0);   
 		  break;
       default:
   		  switchPhase(0, 0, 0, 0, 0, 0);    
-          Serial.println("case:error");
 		  break;
   }
 }
